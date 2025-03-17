c2f4c75607234b5bb7e0111227b604f510977723
manonlespes <manonlespes@hotmail.fr>
2025-03-17 07:47:44 +0100

    feat(app): work on handleChangePassword logic + display message + rework button focus a11y

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import './App.css';
 import { Label } from './components/Label';
 import { PasswordMessage } from './components/PasswordMessage';
@@ -16,13 +16,34 @@ export const defaultPasswordValidation: PasswordValidationType = {
 export const App: React.FC = () => {
   const [passwordStrength, setPasswordStrength] =
     useState<PasswordValidationType>(defaultPasswordValidation);
+  const [isValid, setIsValid] = useState(false);
 
-  const signIn = () => {
-    //do something here
+  const signIn = (formData: FormData) => {
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    if (email && password) {
+      //TODO remove console.log
+      console.log('sign in', email, password);
+    }
   };
 
-  const handleChangePassword = () => {
-    //do something here
+  const handleChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    const password = e.target.value;
+
+    const validations: Record<keyof PasswordValidationType, boolean> = {
+      isCharLength: /^.{8,}$/.test(password),
+      isCharSpecial: /[#.?!@$%^&*\-]/.test(password),
+      isDigit: /(?=.*?[0-9])/.test(password),
+      isLowerCase: /(?=.*?[a-z])/.test(password),
+      isUppercase: /(?=.*?[A-Z])/.test(password),
+    };
+
+    //update state
+    setPasswordStrength(validations);
+
+    // check if every validation is true
+    setIsValid(Object.values(validations).every(Boolean));
   };
 
   return (
@@ -66,7 +87,12 @@ export const App: React.FC = () => {
               />
             </div>
 
-            <button className="focus:ring-blue-[#3d59a2] w-full rounded-md bg-gray-500 p-2 text-sm text-white shadow-md ring-offset-2 transition-all ease-in hover:bg-gray-600 focus:outline-none focus:ring active:bg-gray-700 disabled:border-blue-200 disabled:bg-blue-200 disabled:text-slate-800 disabled:shadow-none">
+            <button
+              className="focus:ring-blue-[#3d59a2] text-bolder w-full rounded-md bg-gray-500 p-2 text-sm text-white shadow-md ring-offset-2 transition-all ease-in hover:bg-gray-600 focus:bg-gray-600 focus:outline-none focus:ring active:bg-gray-700 disabled:border-blue-200 disabled:bg-blue-200 disabled:text-black disabled:shadow-none"
+              disabled={!isValid}
+              aria-disabled={!isValid}
+              title={!isValid ? 'disabled' : ''}
+            >
               Register
             </button>
           </form>
