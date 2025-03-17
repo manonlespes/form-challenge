bdd8c4f4627243e9f482154894c47302e542c472
manonlespes <manonlespes@hotmail.fr>
2025-03-17 07:17:15 +0100

    feat(simple-input): create simple input field component + import components into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,59 @@
 import './App.css';
+import { Label } from './components/Label';
+import { SimpleInput } from './components/SimpleInput';
 
 export const App: React.FC = () => {
-  return <h1 className="">Hello World</h1>;
+  const signIn = () => {
+    //do something here
+  };
+
+  const handleChangePassword = () => {
+    //do something here
+  };
+
+  return (
+    <main>
+      <h1 className="my-6 p-6 text-center text-xl font-bold sm:text-2xl md:text-4xl">
+        Studio ACB - Form Challenge
+      </h1>
+
+      <section className="m-auto block w-full px-5 sm:max-w-[50rem]">
+        <div className="m-auto block w-full rounded-lg bg-white p-6 shadow-md sm:max-w-96">
+          <form action={signIn} className="flex flex-col gap-5">
+            <Label htmlFor="email">
+              Email (ex: jane@doe.de)
+              <SimpleInput
+                id="email"
+                name="email"
+                type="email"
+                placeholder="jane@doe.de"
+                autocomplete="on"
+                required
+              />
+            </Label>
+
+            <div className="flex flex-col gap-3">
+              <Label htmlFor="password">
+                Password
+                <SimpleInput
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="Enter a secured password"
+                  handleChange={handleChangePassword}
+                  required
+                />
+              </Label>
+            </div>
+
+            <button className="focus:ring-blue-[#3d59a2] w-full rounded-md bg-gray-500 p-2 text-sm text-white shadow-md ring-offset-2 transition-all ease-in hover:bg-gray-600 focus:outline-none focus:ring active:bg-gray-700 disabled:border-blue-200 disabled:bg-blue-200 disabled:text-slate-800 disabled:shadow-none">
+              Register
+            </button>
+          </form>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/components/SimpleInput.tsx
@@ -0,0 +1,27 @@
+import { SimpleInputType } from '../types/form';
+
+export const SimpleInput = (props: SimpleInputType) => {
+  const {
+    id,
+    type,
+    name,
+    autocomplete = 'off',
+    placeholder,
+    required,
+    handleChange,
+  } = props;
+  return (
+    <>
+      <input
+        className="border-1 focus:ring-blue-[#3d59a2] w-full rounded-md border border-black px-2 py-[0.375rem] text-sm font-normal text-black shadow-sm transition-all ease-in placeholder:text-[#3d59a2] invalid:border-red-500 invalid:text-red-600 focus:outline-none focus:ring focus:ring-offset-2 focus:invalid:border-red-500 focus:invalid:ring-red-500"
+        id={id}
+        type={type}
+        name={name}
+        autoComplete={autocomplete}
+        placeholder={placeholder}
+        required={required}
+        onChange={handleChange}
+      />
+    </>
+  );
+};
