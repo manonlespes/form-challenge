ed7051214f6f6831af1e9b9257652012bc72bde1
manonlespes <manonlespes@hotmail.fr>
2025-03-17 08:19:25 +0100

    feat(label): add type + adapt component for re-usability

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,13 +1,17 @@
+import clsx from 'clsx';
 import { LabelType } from '../types/form';
 
 export const Label = (props: LabelType) => {
-  const { htmlFor, children } = props;
+  const { htmlFor, children, moreClassname } = props;
+
+  const style = clsx(
+    'flex gap-2 text-sm font-medium text-black',
+    moreClassname
+  );
+
   return (
     <>
-      <label
-        className="flex flex-col gap-2 text-sm font-medium text-black"
-        htmlFor={htmlFor}
-      >
+      <label className={style} htmlFor={htmlFor}>
         {children}
       </label>
     </>
--- src/types/form.ts
+++ src/types/form.ts
@@ -14,6 +14,7 @@ export type SimpleInputType = {
 export type LabelType = {
   htmlFor: string;
   children: ReactNode;
+  moreClassname?: string;
 };
 
 export type PasswordValidationType = {
